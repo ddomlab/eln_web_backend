3f1963c73d9f0ffabb0bac8f5acb6e809b6f141b
12buntu <kylemhollars@gmail.com>
2025-05-23 15:05:36 -0400

    added almost everyhting -- forgot to incrementally commit but it should work now!

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import cross_origin
+import print_handling
+from datetime import datetime
+import json
 import search_process
 import eln_packages_common.resourcemanage as resourcemanage
 
@@ -37,6 +40,70 @@ def search():
     print("Results:", results)
     return jsonify(results)
 
+
+@app.route('/print', methods=['POST'])
+@cross_origin(origins="http://localhost:8000")
+def print_registry():
+    data = request.get_json()
+    ids = data.get('id', [])
+    if len(ids) == 0:
+        return jsonify({"error": "No IDs provided"}), 400
+    if not isinstance(ids, list):
+        return jsonify({"error": "Expected a list of IDs"}), 400
+
+    print_handling.add_item([int(x) for x in ids])
+    print("Printing items with IDs:", ids)
+    
+    return send_from_directory(app.static_folder, "print.pdf")
+
+@app.route('/associate', methods=['POST'])
+@cross_origin(origins="http://localhost:8000")
+def associate():
+    data = request.get_json()
+    ids = data.get('id', [])
+    exp_id = data.get('exp_id')
+    if len(ids) == 0:
+        return jsonify({"error": "No IDs provided"}), 400
+    rmn = rm()
+    if not isinstance(ids, list):
+        return jsonify({"error": "Expected a list of IDs"}), 400
+    for id in ids:
+        rmn.experiment_item_link(exp_id, id)
+    return "Success", 200
+
+@app.route('/mark_open', methods=['POST'])
+@cross_origin(origins="http://localhost:8000")
+def mark_open():
+    data = request.get_json()
+    ids = data.get('id', [])
+    if len(ids) == 0:
+        return jsonify({"error": "No IDs provided"}), 400
+    rmn = rm()
+    if not isinstance(ids, list):
+        return jsonify({"error": "Expected a list of IDs"}), 400
+    for id in ids:
+        body = rmn.get_item(id)
+        metadata = json.loads(body["metadata"])
+        if metadata["extra_fields"]["Opened"]["value"] != "":
+            return jsonify({ "error" : f"Item {id} already marked as opened on {metadata['extra_fields']['Opened']['value']}"}), 400
+        metadata["extra_fields"]["Opened"]["value"] = datetime.now().isoformat()[:10]
+        rmn.change_item(id, {"metadata": json.dumps(metadata), "status": 4})
+    return "Success", 200
+
+@app.route('/mark_empty', methods=['POST'])
+@cross_origin(origins="http://localhost:8000")
+def mark_empty():
+    data = request.get_json()
+    ids = data.get('id', [])
+    if len(ids) == 0:
+        return jsonify({"error": "No IDs provided"}), 400
+    rmn = rm()
+    if not isinstance(ids, list):
+        return jsonify({"error": "Expected a list of IDs"}), 400
+    for id in ids:
+        rmn.change_item(id, {"status": 5})
+    return "Success", 200
+
 @app.route('/template', methods=['GET'])
 @cross_origin(origins="http://localhost:8000")
 def get_template():
@@ -48,7 +115,6 @@ def get_template():
     except Exception as e:
         print("Error initializing Resource_Manager:", e)
         return jsonify({"status": "error", "error": str(e)}), 400
-
 @app.route('/add_resource', methods=['POST'])
 @cross_origin(origins="http://localhost:8000")
 def add_resource():
@@ -76,4 +142,4 @@ def not_found(e):
     return f"404 Not Found: {request.path}", 404
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ print_handling.py
@@ -0,0 +1,24 @@
+from eln_packages_common.resourcemanage import Resource_Manager
+from pypdf import PdfWriter
+import io
+from pathlib import Path
+
+rm = Resource_Manager()
+current_dir = Path(__file__).parent
+temp_path = str(current_dir / "static" / "print.pdf")
+
+
+def add_item(ids: list[int]):
+    merger = PdfWriter()
+    for id in ids:
+        # looks at all the files uploaded to the item, but only selects ones named label.pdf
+        for file in rm.get_uploaded_files(id):
+            if file.to_dict()["real_name"] == "label.pdf":
+                new_label = io.BytesIO(  # reads the file as binary
+                    rm.uploadsapi.read_upload( # type: ignore
+                        "items", id, file.to_dict()['id'], format="binary", _preload_content=False
+                    ).data
+                )
+                merger.append(new_label)
+        merger.write(open(temp_path, "wb"))
+        merger.close()  # closes the merger --- potentially there would be benefit to leaving the merger open until write_labels but then this would have to be another objectprinterqueue
